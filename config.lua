f9c9f9eb6feb894037befdf649ad745e0a9dd76b
Patrick Loyd <patrickloyd789@gmail.com>
2022-11-19 23:53:17 -0500

    remove depracated setting

--- config.lua
+++ config.lua
@@ -76,7 +76,6 @@ keymap("v", "S", ":s///g<left><left><left>", no_remap)
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
